9eceb5a1818540f36bbfe79612d90e4aeb4bba8c
Ganesh98153 <61342149+Ganesh98153@users.noreply.github.com>
2024-12-23 11:18:47 +0545

    Update main.py
    Code Update after Site Update

--- main.py
+++ main.py
@@ -23,45 +23,22 @@ formatted_time = now.strftime("%I:%M:%S %p")
 
 print(formatted_time)
 
-json_data = {
-    'userName': 'ganesh.tiwari@Bitskraft.com',
-    'password': 'Bits@98153', #Enter your password here
-    'strategy': 'password',
-}
-
-
-response = requests.post('https://bitskraft.rigohr.com/v1/auth/signin', json=json_data)
-
-if response.status_code == 200:
-    json_response = response.json()
-    token = json_response.get('Token')
-
-    cokies_dict = response.cookies.get_dict()
-    #print("Cokies : ", cokies_dict)
-    print()
-    
-    #print(token)
-
-else :
-    print(f"Request failed with status code : {response.status_code}")
-
 headers = {
-    'authority': 'bitskraft.rigohr.com',
     'accept': 'application/json, text/plain, */*',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'authorization': f'bearer {str(token)}',
+    'accept-language': 'en-US,en;q=0.9',
     'content-type': 'application/json',
-    #'cookie': '.AspNetCore.Identity.Application=CfDJ8MfsdsRL3gtLhbygVOOENYrx9BsWKZgQDsKCFZTefPIRfZbcUEZzX3ajhzIGHGeo9tojbmvT77-QLPnb2Lj7kkwbA7t7fRTOQmYx9rNU44lSsHFhzoskQa-roB4mbWAVTzXGaNzQVgDo9IctB_W9L0Igc5mWa4ZsjZULCdOChykh0dfOtG1BpW8ZB5iyBwl7bQGZ1Da7ynAMXrPrqMXHpIo7FQ264U8BRBrnt8uyek2ukxxwSh0eOVds3SKOlBlpbkL2kUDZqKWbj2orPc0aqoHaFqC1f4-cWg0or1ZAVfniYDlstDsYO4pEshiBRNAAXqSSQiwZm7k9H1NR30k3u8Dn9dm75zeoJy1ufZHioXjtvhSKLZlhc51GoUONX59ElliyqKXNTT4wAJb3JxwQM-bw6db1IfGx9sGvWLXH8wrzwmA1ClTuy24qMdgmg1--tUrIoRzZfJ8kg5aR_zegJi0uExf_0zP_F8K7c18pYmAnYlP90CqQPVeKPCHDrEFIOibnQ8HVIg3tYjjLpFaoUZzlwhOT3nRjeIkBNHx5WFnkmMQyaxVOMc1niUmht7-Zn5baZP4GfBlhrLpAvx1ev0BFeo7gMzRNFbwO2xwGXT5WkijWnIE1JtsW_PiX2Cklzc1X8VP6pyMwwgo7-DuR8SDgzdgeO8XlOUOtrR9YCFz37_tR121Gd42sWTEZMAGUxBabCRfDUBY-4nshGCUvRXk; .AspNet.SharedCookie=CfDJ8MfsdsRL3gtLhbygVOOENYp52Gyo63vYiGj-jn8wZZo6nw0pLZ8_C-gkQZsb15mubDCyKaMczeVH-bUhyDJJnV2czdxuju-KfZRUDTM5B0QD4U3JAE0I-vkb6Ltqasu3QA6ITUjbdxRZ25pfkDwvvaVb7495NJ-Og63SvT059or_dXcbF5ClHic24JL2vznQRjgH-q6Fu5AhOjrEJ7sIAf3RJt-xzST0WHSgkXypffXnR3ygkIiBtqRkBxNWUmKiaCarM7X1jWviw4E8yfxpxNjgqKIRVO8pwoNEUtylhX0b4B6qoL9TdcwCTw4aGVJ2Qo3BObTnhGQlZFkgmpAXnBFzyCTUD_SNraSd99ITLj93jvvptRkXihWluh41wNF2A_LuoNyzFdnR6YQeqmziQNIclVcACIhYhg2Zsq-jCPOCLdbDYab5PSyvRtP3m9syPXE0MmDV69tzHn_4mT-Y4fMTFA5yeJLX3O5Utf_1Ktgy4tDYXnbjwXdKuCTmPPJye8g9WW92jc6fV7Ep39M2MZb1mxDi3MGUml01EFh19l4I9j7UdtjJ7QywLcDnHT9PotW618irLeFvdzYdoUicolf0O_U5-LLK_redLzhHqOnEOZ44mfm9s1X5QrFlg7DnLL3bXEbxvh3J3PJeeQjLivgLcDELMDHStrv38Km8jCW6',
-    'loading': 'false',
-    'origin': 'https://bitskraft.rigohr.com',
-    'referer': 'https://bitskraft.rigohr.com/employee/personal-information',
-    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'cookie': 'OpenIddict.Client.State.SOXoJA40ps0juYgoPDIQb-D01UKp1UAdnMVIlmph-bU=AQAAACtaaVBqdzA2amVPc3FldEFXYWdDYTlhRHBmUFVwcGVDVlJHTUVuTUh1RXE0; canary=never; .RigoHR.SharedCookie=CfDJ8Cx-cLqiER5NtW0nQuVCfk0kA20Xwn5vc2jkSSVAMv68m0acY62nYByB8FLGiw2KI00dQ82nQPOxUmecB0ehTnUxV6ioXIojd3Y5VAObpezr1fyEn2mdSqBFg3Bk599WThE27WWL7iy5m0dSD9l2a-OXAuxsmV_nv2A0PNjvcLYRUtKKTerzlBhdzXU4NNOkcPS8ncwjKawCjNw0BDvV9uBO0_yAD6qBL4rcYkTTwp82NCJmieBnqsEJsFbYAUxzgDhLUdObp7sD6x5_V_LrzEsOTFkozRb5hQ01rPXQh-Dl9kfs3pE2jJWXEA1HjpQrB-pGdZz_23Lz9C8pfN0RNnPrJhrznsZrGrvDWuCK2QyJIyN2tMgOvW7_2F9VlysrJk301SEpXhyyQ3x2vMHBwpRimc_p8coGwqgcZrw2DkfGcKs0W6n1DAglgemwo3N1x87YnRFCDgZ5NUamtuEglllAtFHF4ebvAn7AG48wsJW9utmz4w0NkSgBXYAeUtkzLbExKTJD-cKd0rqqJ6uFkfvhPyItVDPgb17QWqwQCXYPY-OtEsakuLAnBeLwc9pOxLUxqQNOxbkYb3Zn7yQp7fTm5uMNZdSqCqh12ns5RFJCz5dWnQ4WLaMWcHl3vwoJuzuEH2m8GE7wwTX2pVqy3ApXGzydCNJ6rCa11ZwwOX2wZ-sbxmRs-w4ykJbmq1u59rqsMxYpw-nrpQXs5Hjxwfum1P7n1FKQsU8LVL4W2MtiEOeRedyc12w7Yq2QkUU7SQQFEQe895XwLfzs34cDPL8PnGULPWH42VZwC-CxB-Liv1wYINMiHyrLw_u9kr1bSNaoXJQQFxuKkjTRQBRiPnv0WnzezoWCIhWpF-aZNlP3n4A6toR2uEKrJzagmHUbMuOjntFpWXh8DiSCZvWgw_DxOkayYGwpmpxUdJQxCsAGKtSh9JtcQOWkvrdQCe8YaHDQnk32lMOBQqkIv256zCGSHCpf2D9P8mwJ2XaljhHZt661C4CA2HOnMWYlrPsgyjvgo98p_duAaQJvQQ4GV_iHXCkVor9-wqWKdv_i4FCJqAFWAy-foX9s4R1Mzjkx_w',
+    'origin': 'https://app.rigohr.com',
+    'priority': 'u=1, i',
+    'referer': 'https://app.rigohr.com/hr/clock/in',
+    'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'sec-fetch-site': 'same-site',
+    'tenantid': '5764af9a-5073-44db-9af1-9c6a6d29a15a',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 }
 #isBreakIn
 params = {
@@ -75,7 +52,7 @@ json_data = {
 
 response = requests.post(
     'https://api.app.rigohr.com/v1/leave-time/attendance/manual',
-    cookies=cokies_dict,
+    #cookies=cokies_dict,
     params=params,
     headers=headers,
     json=json_data,
